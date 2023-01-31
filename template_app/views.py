5b4538703388c98c9933d8cc371fae33066b37ed
Oliver Speir <oliverspeir9@gmail.com>
2023-01-30 17:51:16 -0700

    lab 31 complete

---
+++ template_app/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .serializers import ItemSerializer
+from .models import Item
+
+
+class ItemList(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
